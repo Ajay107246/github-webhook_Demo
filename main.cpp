3a3038d6d519ef07c48325727009ce4857bebfba
Ajay Shimpi <shimpi.ajay.24@gmail.com>
2024-08-03 17:08:35 +0200

    commmit#2: Topic2, web-hook, webhook setting done in github and random url generated using choco install ngrok

--- main.cpp
+++ main.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     cout << "this is demo repo to do anything from Jenkins with GitHub!" << endl;
+    cout << " to fo GitHub SCM polling, you need ngrok" << endl;
+    cout << "to installed using choco install ngrok" << endl;
+    cout <<  "and use ngrok 8080 or command = "<< endl;
+    cout << "ngrok will create dummy https url for you" << endl;
+    cout << "use that url/github-webhook/ in your github repo -> setting -> webhook" << endl;
     return 0;
 }

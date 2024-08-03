789fe59ac55a07fd8cb5f9d2893b1a5e88856715
Ajay Shimpi <shimpi.ajay.24@gmail.com>
2024-08-03 17:14:36 +0200

    commit#3, Topic3, ngrok command updated and guide.txt

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "this is demo repo to do anything from Jenkins with GitHub!" << endl;
     cout << " to fo GitHub SCM polling, you need ngrok" << endl;
     cout << "to installed using choco install ngrok" << endl;
-    cout <<  "and use ngrok 8080 or command = "<< endl;
+    cout <<  "and use ngrok 8080 or command = ngrok http 8080 --host-header=\"localhost:8080\"" << endl;
     cout << "ngrok will create dummy https url for you" << endl;
     cout << "use that url/github-webhook/ in your github repo -> setting -> webhook" << endl;
     return 0;
